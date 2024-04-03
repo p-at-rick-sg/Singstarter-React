af4da0348578515cf492c3220449a21017c63404
ga-patrick-kittle <patrick.kittle@gmail.com>
2024-04-03 18:01:28 +0800

    added the new project component - file upload working OK

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,14 @@ import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Success from './components/Success';
 import Cancel from './components/Cancel';
-
+import AddProject from './components/AddProject';
 //Context Imports (may need to set the theme here if we want light/dark mode setup)
 
 //MUI Stuff
 //Create the theme and apply it around the whole app
 import {createTheme, ThemeProvider} from '@mui/material';
-import AddProject from './components/AddProject';
+import {LocalizationProvider} from '@mui/x-date-pickers';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 
 const userTheme = createTheme({
   palette: {
@@ -40,23 +41,25 @@ const userTheme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={userTheme}>
-      <Fragment>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Navigate to="home" />} />
-          <Route path="home" element={<LandingPage />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="signin" element={<Signin />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/member" element={<MemberPage />} />
-          <Route path="/member/add" element={<AddProject />} />
-          <Route path="/project" element={<ProjectPage />} />
-          {/* move these routes to be under the a master member page?? */}
-          <Route path="/success" element={<Success />} />
-          <Route path="/cancel" element={<Cancel />} />
-        </Routes>
-        <Footer />
-      </Fragment>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Fragment>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Navigate to="home" />} />
+            <Route path="home" element={<LandingPage />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="signin" element={<Signin />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/member" element={<MemberPage />} />
+            <Route path="/member/add" element={<AddProject />} />
+            <Route path="/project" element={<ProjectPage />} />
+            {/* move these routes to be under the a master member page?? */}
+            <Route path="/success" element={<Success />} />
+            <Route path="/cancel" element={<Cancel />} />
+          </Routes>
+          <Footer />
+        </Fragment>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -12,78 +12,239 @@ import {
   Box,
   Container,
   Typography,
+  CssBaseline,
+  ListItem,
+  List,
+  ListItemIcon,
+  ListItemButton,
+  ListItemText,
 } from '@mui/material';
+import PhotoCameraBackIcon from '@mui/icons-material/PhotoCameraBack';
+
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import {Dayjs} from 'dayjs';
 
 //Context
 import {useUser} from '../hooks/useUser';
 
+//Main Component Function
 const AddProject = () => {
-  const {user} = useUser();
+  const fetchData = useFetch();
+  const {user, setPageTitle} = useUser();
   const [newProject, setNewProject] = useState({
     title: '',
-    details: '',
+    description: '',
     target: 0,
-    endDate: Date.now(),
-    images: [],
+    images: null,
   });
+  const [endDate, setEndDate] = useState(null);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    setPageTitle('Add Project');
+  }, []);
 
   const handleChange = e => {
-    console.log('change function');
     setNewProject({...newProject, [e.target.name]: e.target.value});
+  };
+
+  const handleFile = async e => {
+    setImage(e.target.files[0]);
+  };
+
+  const uploadImage = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append(
+      'Authorization',
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNvbnRyaWJ1dG9yQHRlc3QuY29tIiwicm9sZSI6ImNvbnRyaWJ1dG9yIiwiaWQiOiI2NzAwZGU2YjFmZDExNjJhYWUyMmZmMzAiLCJpYXQiOjE3MTIxMzY5MDcsImV4cCI6MTcxMjEzODcwNywianRpIjoiOWRlYWU4MjYtMWYzOC00NGJkLTkyYzYtZWZhMDRjZjM5NTFkIn0.CyTUDaW7S77Cpu5AClPYQGB4fwilZa4Uy3rnVNCOeNk'
+    );
+    const formdata = new FormData();
+    formdata.append('image', image, 'test-image.jpg');
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formdata,
+      redirect: 'follow',
+    };
+
+    const result = await fetch(
+      'http://localhost:7001/api/projects/uploadAsset/6700ddf51fd1162aae22ea20',
+      requestOptions
+    );
+    const data = await result.json();
+    console.log(data);
+  };
+
+  const addProject = async () => {
+    const body = {
+      title: newProject.title,
+      description: newProject.description,
+      target: newProject.target,
+    };
+    if (endDate) body.endDate = endDate;
+    console.log(body);
+    const result = await fetchData(
+      '/api/projects',
+      'PUT',
+      body,
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNvbnRyaWJ1dG9yQHRlc3QuY29tIiwicm9sZSI6ImNvbnRyaWJ1dG9yIiwiaWQiOiI2NzAwZGU2YjFmZDExNjJhYWUyMmZmMzAiLCJpYXQiOjE3MTIxMzY5MDcsImV4cCI6MTcxMjEzODcwNywianRpIjoiOWRlYWU4MjYtMWYzOC00NGJkLTkyYzYtZWZhMDRjZjM5NTFkIn0.CyTUDaW7S77Cpu5AClPYQGB4fwilZa4Uy3rnVNCOeNk'
+    );
+    uploadImage();
   };
 
   const handleSubmit = e => {
     console.log('submit function');
+    e.preventDefault();
+    addProject();
+    const formData = new FormData();
+    formData.append(newProject.title, image);
+  };
+
+  const handleCancel = e => {
+    //return the the member page once we have one
   };
 
   return (
     <>
-      <Container component="main" maxWidth="m">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            border: '1px solid black', //remove once layout looks OK
-          }}>
-          {/* <Avatar sx={{m: 1, bgcolor: 'primary.main'}}>
+      <CssBaseline>
+        <Container component="main" maxWidth="m">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              border: '1px solid black', //remove once layout looks OK
+            }}>
+            {/* <Avatar sx={{m: 1, bgcolor: 'primary.main'}}>
             <LockOutlinedIcon sx={{color: 'inherit'}} />
           </Avatar> */}
-          <Typography component="h1" variant="h5">
-            Adding Your Great idea
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} sx={{mt: 0}}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  required
-                  id="title"
-                  label="Project Title"
-                  name="title"
-                  autoFocus
-                  value={newProject.title}
-                  onChange={handleChange}
-                />
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  required
-                  id="title"
-                  label="Project Title"
-                  name="title"
-                  autoFocus
-                  value={newProject.title}
-                  onChange={handleChange}
-                />
-                <TextField type="file" />
+            <Typography component="h1" variant="h5" sx={{color: 'primary.main'}}>
+              Adding Your Great idea
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} sx={{mt: 0}}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    required
+                    id="title"
+                    label="Project Title"
+                    name="title"
+                    autoFocus
+                    value={newProject.title}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    required
+                    id="target"
+                    label="Funding Target"
+                    name="target"
+                    autoFocus
+                    value={newProject.target}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    required
+                    id="description"
+                    label="Project Description"
+                    name="description"
+                    autoFocus
+                    value={newProject.description}
+                    onChange={handleChange}
+                    multiline
+                    minRows={8}
+                    maxRows={8}
+                  />
+                </Grid>
+
+                <Grid
+                  container
+                  spacing={2}
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="center">
+                  <Grid item xs={12} sm={4}>
+                    <TextField
+                      type="file"
+                      margin="normal"
+                      sx={{marginTop: '5px'}}
+                      required
+                      id="image"
+                      label="Upload Images"
+                      name="image"
+                      autoFocus
+                      value={newProject.image}
+                      onChange={handleFile}
+                    />
+                  </Grid>
+                  <Grid item xs={6} sm={3}>
+                    <TextField
+                      margin="normal"
+                      fullWidth
+                      required
+                      id="imageDescritpion"
+                      label="Image Description"
+                      name="imageDescritpion"
+                      autoFocus
+                      value={newProject.imageDescription}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <DatePicker
+                      sx={{}}
+                      id="endDate"
+                      label="End Date (Optional)"
+                      name="endDate"
+                      autoFocus
+                      value={endDate}
+                      onChange={setEndDate}
+                    />
+                  </Grid>
+                </Grid>
               </Grid>
-            </Grid>
+              <Grid
+                container
+                spacing={2}
+                direction="row"
+                alignItems="center"
+                justifyContent="center">
+                <Grid item xs={8} sm={5}>
+                  <Button
+                    type="cancel"
+                    fullWidth
+                    variant="outlined"
+                    sx={{mt: 3, mb: 2, margin: '5px'}}
+                    //   disabled={addProject ? true : false}
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
+                <Grid item xs={8} sm={5}>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt: 3, mb: 2, margin: '5px'}}>
+                    Add my Project
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </CssBaseline>
     </>
   );
 };
