cc028a20d14a9ab0b05a33538d9bc101e3b1a9eb
kr2 <rat.dog.girl@hotmail.com>
2024-04-09 19:44:11 +0800

    fix projectpage route when not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -126,7 +126,7 @@ function App() {
               <Route path="home" element={<LandingPage />} />
               <Route path="signup" element={<Signup />} />
               <Route path="signin" element={<Signin />} />
-              <Route path="/project" element={<ProjectPage />} />
+              <Route path="/project/:id" element={<ProjectPage />} />
               <Route path="*" element={<NotFoundPage />} />
             </Routes>
             <Footer />
